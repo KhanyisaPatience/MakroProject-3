b977244581ee48da3fc5a46c751082261d4f29d3
KhanyisaPatience <kngalwana@yahoo.com>
2019-11-01 12:21:26 +0200

    changes made

--- person/Customer.java
+++ person/Customer.java
@@ -1,13 +1,48 @@
-class Customer extends Person{
-cardNumber;
-username;
-password;
+import java.util.scanner;
+
+class Customer extends Pesrson{
+
+  private int cardNumber;
+  private string userName;
+  private int password;
+
+ public Customer(){}
+
+ public Customer(int cardNumber, string userName,int password){
+}
+public void setCardNumber(int carNumber){
+this.cardNumber = cardNumber;
+}
+public int getCardNumber(){
+return cardNumber;
+}
+public void setUserName(string userNamer);
+this.userName = userName;
+}
+public String getUserName();
+return userName;
+}
+public void setPassword(int password){
+password.password = password;
+}
+public int getPassword(){
+return password(){
+}
 
 
-purchase();
-return();
-cancel();
+  Scanner sc = new Scanner(System.in);
+
+public void purchase(){
+
+}
+
+
+public void selectProducts(){
+   Scanner sc =Scanner(System.in);
+
+  System.out.println("")
 
 
 
+}
 }
